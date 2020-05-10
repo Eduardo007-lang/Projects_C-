94e8ec1536a2e33d1f489a5cb7ecd10dd64eae1a
Eduardo007-lang <53060876+Eduardo007-lang@users.noreply.github.com>
2020-05-10 17:13:37 -0300

    Add files via upload

---
+++ Exercico5/Exercico5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercico5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(vet[0]);
+            int quantidade = int.Parse(vet[1]);
+
+            double total;
+
+            if (codigo == 1)
+            {
+                total = quantidade * 4.00;
+            }
+            else if (codigo == 2)
+            {
+
+                total = quantidade * 4.50;
+            }
+            else if (codigo == 3)
+            {
+
+                total = quantidade * 5.00;
+            }
+            else if (codigo == 4)
+            {
+
+                total = quantidade * 2.00;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
